6a4034839c70caaabea345d177a1f5dee72e7416
Ömer AK <107984157+oomerak@users.noreply.github.com>
2022-07-10 20:27:39 +0300

    Global AI Hub Project 1 - 10.07.2022

--- dracarys_project_1.py
+++ dracarys_project_1.py
@@ -1,6 +1,8 @@
-#TRAINING
+from telnetlib import STATUS
 import pandas as pd
 import numpy as np
+
+
 
 student_number2 = []
 first_name2 = []
@@ -20,15 +22,11 @@ def conv_grade(points2):
     else:
         return 'F'
 
-
 def situation2(point):
     if point<50:
         return "FAILED"
     else:
         return "PASSED"
-
-#Check the func
-#conv_grade(70)
 
 
 #Define a function. That takes student name and points.
@@ -36,44 +34,79 @@ def message(student_number, first_name, last_name, points, lesson):
     print(f"Student number : {student_number}, {first_name} {last_name} scored {points}/100 points in {lesson} and got the letter '{conv_grade(points)}' ")
 
 
-#Title for MATH lesson
-print("Application for MATH Lesson \n-------------------\n")
+#Converting the Dataframe(csv) file to excel (xlsx) file
 
+def convert2Excel1():
 
-input_choice = input("Do you want to enter student? You must enter only 'yes' or 'no' \n")
-for i in input_choice:
-    if input_choice == ("yes"):
-
-        student_number = int(input("Please enter student's ID: "))
-        first_name = input("Please enter student's first name: ")
-        last_name = input("Please enter student's last name: ")
-        point = int(input("Please enter student's point: "))
-
-        lesson_grade = conv_grade(point)
-        situation = situation2(point)
-      
-        newStudentData = first_name + "," + last_name + "," + str(student_number) + "," + str(point) + "," + lesson_grade + "," + situation + "\n"
-        student_report_file = open("student_report.csv" , "a") # a: Writing to new line (end) | w: Deleting first line and writing the new value
-
-        student_report_file.write(newStudentData)
-        
-        input_choice = input("Do you want to enter another student? 'yes' or 'no' \n ")
-        
-    else: 
-        print("The dataframe of students are saved to student_report.csv file.\nDataframe has been converted to excel file.")
-        break
+    df = pd.read(r"student_report.csv")
+    c2e = df.to_excel(r"student_report.xlsx" , index = None , header = False)
 
 
 
 
     
-"""
-try:
-    students_file = open("student_report.csv", 'r')
-    print('There is an existing report card for this student')
-except:
-    students_file = open("student_report.csv", 'w')
-    students_file.write(message(15, "Cagla", "Kamaci", 29, "English"))
 
-students_file.close()
-""" 
+#Title for MATH lesson
+print("Application for MATH Lesson \n-------------------------------\n")
+
+
+process2 = True
+
+
+while process2 == True:
+    try:
+        studentamount = int(input("Please enter number of students\n"))
+
+    except ValueError:
+        process2 = True
+
+    else:
+        process2 = False
+
+
+for i in range (studentamount):
+    process = True
+
+    while process == True:
+        try:
+            student_number = int(input("Please enter student's ID: "))
+            first_name = input("Please enter student's first name: ")
+            last_name = input("Please enter student's last name: ")
+            point = int(input("Please enter student's point: "))
+
+            
+        except TypeError:
+            print('Oh no! A TypeError has occured')
+            process = True
+
+        except ValueError:
+            print('A ValueError occured!')
+            process = True
+
+        else:
+            print('No exception')
+            process = False
+
+    
+    if process == False:
+        lesson_grade = conv_grade(point)
+        situation = situation2(point)
+
+        newStudentData = first_name + "," + last_name + "," + str(student_number) + "," + str(point) + "," + lesson_grade + "," + situation + "\n"
+
+        
+        student_report_file = open("student_report.csv" , "a") # a: Writing to new line (end) | w: Deleting first line and writing the new value
+
+        student_report_file.write(newStudentData)
+    
+
+
+
+df = pd.read_csv(r"student_report.csv")
+writer = pd.ExcelWriter('Proje1.xlsx')
+df.to_excel(writer)
+ 
+writer.save()
+
+
+print("The dataframe of students are saved to student_report.csv file.\nDataframe has been converted to excel file.")
